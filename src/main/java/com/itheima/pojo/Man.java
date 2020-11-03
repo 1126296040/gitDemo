4e477bd5ae68223050c9b1a1b423a46ab7bd3773
1126296040 <1126296040@qq.com>
2020-11-03 19:54:18 +0800

    第一次提交

---
+++ src/main/java/com/itheima/pojo/Man.java
@@ -0,0 +1,14 @@
+package com.itheima.pojo;
+
+/**
+ * @Description:
+ * @Program: spring
+ * @Author: 人称Java之夫
+ * @Date: 2020/11/3 0003 19:41
+ * @version: 1.8
+ */
+
+public class Man {
+    private String name;
+    private String sex;
+}
